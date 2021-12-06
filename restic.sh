e6d84ba279364f170847b986a4dccd56c3718f53
Florian Hoss <mail@florianhoss.de>
2021-12-06 08:36:19 +0100

    create restic.sh, a script to help manage restic commands

---
+++ restic.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+action=$1
+repo=$2
+option=$3
+
+checkAllEnvironmentVariables() {
+    local envError=false
+    [ "$PCLOUDLOCATION" == "" ] && envError=true
+    if [ "$envError" = true ]; then
+        printError "Some environmet variables are not set"
+        exit 1
+    else
+        printSuccess "All environmet variables are set"
+    fi
+}
+
+checkAllEnvironmentVariables
+
+case $action in
+    "snapshots" )
+	    restic -r rclone:pcloud:"$PCLOUDLOCATION""$repo" snapshots --password-file /opt/backup/.resticpwd
+        ;;
+    "remove" )
+	    restic -r rclone:pcloud:"$PCLOUDLOCATION""$repo" forget "$option" --prune --password-file /opt/backup/.resticpwd
+        ;;
+    "keep-last" )
+	    restic -r rclone:pcloud:"$PCLOUDLOCATION""$repo" forget --keep-last "$option" --prune --password-file /opt/backup/.resticpwd
+        ;;
+    "init" )
+	    restic -r rclone:pcloud:"$PCLOUDLOCATION""$repo" init --password-file /opt/backup/.resticpwd
+        ;;
+    "restore" )
+	    restic -r rclone:pcloud:"$PCLOUDLOCATION""$repo" restore "$option" --target /tmp/ --password-file /opt/backup/.resticpwd
+        ;;
+    *)
+        printf "🔔 HOW-TO 🔔\n\n"
+        printf "snapshots 	[repo]\n"
+        printf "remove 		[repo] 		[snapshot-id]\n"
+        printf "keep-last 	[repo] 		[amount]\n"
+        printf "init 		[repo]\n"
+        printf "restore 	[repo] 		[latest/snapshot-id]\n"
+        exit 1
+esac
